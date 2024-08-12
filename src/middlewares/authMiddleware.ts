6f806a88a7858633673e6ce0d72fc225699c8858
Artiom-Evs <artiom.evs@gmail.com>
2024-08-12 13:43:13 +0200

    Fix issue with not awaiting pipeline invoke in authorization middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,7 +2,7 @@ import { CustomContext } from "../customContext";
 
 export default async function authMiddleware(ctx: CustomContext, next: () => Promise<void>) {
     if (ctx.session.auth.isAuthorized)
-        next();
+        await next();
     else 
         await ctx.reply("You are not authorized. Please, use /start to authorize.");
 }
