0b6d18737122dcb1572782e05e5eab69ac2b3935
Artiom-Evs <artiom.evs@gmail.com>
2024-09-27 17:47:41 +0200

    Implement authorized users filtration before running authorization scene

--- src/bot/services/public-commands.service.ts
+++ src/bot/services/public-commands.service.ts
@@ -1,12 +1,16 @@
 import { Command, Update } from "nestjs-telegraf";
 import { Context } from "telegraf";
 import { SceneContext } from "telegraf/typings/scenes";
+import { CustomContext } from "../interfaces/custom-context.interface";
 
 @Update()
 export class PublicCommandsService {
     @Command("start")
-    async start(ctx: SceneContext) {
-        await ctx.scene.enter("authorization");
+    async start(ctx: CustomContext) {
+        if (ctx.session.auth?.isAuthorized)
+            await ctx.reply("You are already authorized!");
+        else
+            await ctx.scene.enter("authorization");
     }
 
     @Command("help")
